187461ec548ba5af26e789303930b4d65a182e0d
jwu26-github <jwu26@punahou.edu>
2024-11-01 09:40:55 -1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,5 +3,21 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(173, 216, 230)
+
+//buildings
+  fill (GRAY)
+rect (40, 100, 40, 400)
+rect (100, 100, 40, 400)
+rect (160, 100, 40, 400)
+rect (220, 100, 40, 400)
+rect (280, 100, 40, 400)
+
+//grass
+fill("green"); 
+rect(0, 350, 400, 400);
+  
+ //sun
+  fill("yellow");
+  circle(400, 50, 100);
 }
